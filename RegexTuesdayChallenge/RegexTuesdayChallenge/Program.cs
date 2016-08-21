e34af84362e49f09d5b959182a0800f71233cf9f
APlusD <azamat.dzhonov@gmail.com>
2016-08-21 17:48:00 +0300

    5-7

--- RegexTuesdayChallenge/RegexTuesdayChallenge/Program.cs
+++ RegexTuesdayChallenge/RegexTuesdayChallenge/Program.cs
@@ -43,10 +43,9 @@ namespace RegexTuesdayChallenge
             //    "$1 <strong>$2</strong>");
 
 
-            
             //Console.WriteLine("Task_3:");
             ///// Select dates between 1000 and 2012. Each month have 30 days.
-            ///// Pattarn pass all test
+            ///// Pattern pass all test
             ///// http://callumacrae.github.io/regex-tuesday/challenge3.html
             ///// Pattern:/^(1\d{3}|20(0\d|1[0-2]))/(0[1-9]|1[02])/(0[1-9]|[1-2]\d|30) (([0-1]\d|2[0-4]):[0-5]\d)(:[0-5]\d|60)?$/
             //FindMatches(text,
@@ -54,18 +53,19 @@ namespace RegexTuesdayChallenge
 
             //Console.WriteLine("Task_4:");
             /////Change only *text* on tag <em>text</em>
-            /////Test on sity, fall in all
+            /////Pattern pass 7 test from 11
             /////http://callumacrae.github.io/regex-tuesday/challenge4.html
             ////// /\*([A-Za-z0-9 ,.]+)\*/g  <em>$1</em>
             //FindAndChange(text,
             //   @"\*([A-Za-z0-9 ]+)\*",
             //   "<em>$1</em>");
 
-            Console.WriteLine("Task_5:");
-            //Find numbers divided by ",."  8,205,500.4672 
-            //Pattern:^\d{1,3}((,| )\d{3})*(.\d+)?$
-            //https://callumacrae.github.com/regex-tuesday/challenge5.html
-            FindMatches(text, @"\b\d{1,3}((,| )\d{3})*(.\d+)?\b");
+            //Console.WriteLine("Task_5:");
+            ////Find numbers divided by ",."  8,205,500.4672 
+            ////Pattern pass 27 test from 31
+            ////Pattern:^\d{1,3}((,| )\d{3})*(.\d+)?$
+            ////https://callumacrae.github.com/regex-tuesday/challenge5.html
+            //FindMatches(text, @"\b\d{1,3}((,| )\d{3})*(.\d+)?\b");
 
             ////Console.WriteLine("Task_6:");
             ///////Find IPv4
@@ -73,10 +73,10 @@ namespace RegexTuesdayChallenge
             ////    @"");
 
             //Console.WriteLine("Task_7:");
-            /////Find links for web-sites 
-            /////Test on sity
+            /////Test pass 38 test from 41
+            /////Pattern: /^(https|http)://(www\.)?[^-][\w-\.]{1,40}[^-.]\.\w{1,10}/?$/
             /////https://callumacrae.github.com/regex-tuesday/challenge7.html
-            //FindMatches(text, @"/\b(www.|(https|http)://(www.)?)[A-Za-z1-9_]{1,20}.\w{1,5}\b/");
+            //FindMatches(text, @"\b(https|http)://(www\.)?[^-][\w-\.]{1,40}[^-.]\.\w{1,10}/?\b");
 
             //Console.WriteLine("Task_8:");
             //FindAndChange(text,
