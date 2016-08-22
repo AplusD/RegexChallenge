0be5fdb50e7346d6f7aa0d192ce7f9d15ff13851
APlusD <azamat.dzhonov@gmail.com>
2016-08-22 22:56:15 +0300

    Modified

--- RegexTuesdayChallenge/RegexTuesdayChallenge/Program.cs
+++ RegexTuesdayChallenge/RegexTuesdayChallenge/Program.cs
@@ -33,55 +33,64 @@ namespace RegexTuesdayChallenge
 
         public static void DoTasks(String text)
         {
-            //Console.WriteLine("Task_1:");
-            /////Mark repeat word by tag <strong> 
-            ///// Pattern pass all test
-            ///// https://callumacrae.github.com/regex-tuesday/challenge1.html
-            ///// Pattern: /\b([a-zA-Z'.\-]+)\s(\1)\b/gi  Change: $1 <strong>$2</strong>
-            //FindAndChange(text,
-            //    @"\b([a-zA-Z'.\-]+)\s(\1)\b",
-            //    "$1 <strong>$2</strong>");
+            Console.WriteLine("Task_1:");
+            ///Mark repeat word by tag <strong> 
+            /// https://callumacrae.github.com/regex-tuesday/challenge1.html
+            /// Pattern: /\b([a-zA-Z'.\-]+)\s(\1)\b/gi  Change: $1 <strong>$2</strong>
+            FindAndChange(text,
+                @"\b([a-zA-Z'.\-]+)\s(\1)\b",
+                "$1 <strong>$2</strong>");
 
+            Console.WriteLine("Task_2:");
+            ///Choose grey colors in CSS 
+            ///http://callumacrae.github.io/regex-tuesday/challenge2.html
+            ///Pattern:
+            FindMatches(text,
+                @"");
 
-            //Console.WriteLine("Task_3:");
-            ///// Select dates between 1000 and 2012. Each month have 30 days.
-            ///// Pattern pass all test
-            ///// http://callumacrae.github.io/regex-tuesday/challenge3.html
-            ///// Pattern:/^(1\d{3}|20(0\d|1[0-2]))/(0[1-9]|1[02])/(0[1-9]|[1-2]\d|30) (([0-1]\d|2[0-4]):[0-5]\d)(:[0-5]\d|60)?$/
-            //FindMatches(text,
-            //@"\b(1\d{3}|20(0\d|1[0-2]))/(0[1-9]|1[02])/(0[1-9]|[1-2]\d|30) (([0-1]\d|2[0-4]):[0-5]\d)(:[0-5]\d|60)?\b");
+            Console.WriteLine("Task_3:");
+            /// Select dates between 1000 and 2012. Each month have 30 days.
+            /// http://callumacrae.github.io/regex-tuesday/challenge3.html
+            /// Pattern:/^(1\d{3}|20(0\d|1[0-2]))/(0[1-9]|1[02])/(0[1-9]|[1-2]\d|30) (([0-1]\d|2[0-4]):[0-5]\d)(:[0-5]\d|60)?$/
+            FindMatches(text,
+            @"\b(1\d{3}|20(0\d|1[0-2]))/(0[1-9]|1[02])/(0[1-9]|[1-2]\d|30) (([0-1]\d|2[0-4]):[0-5]\d)(:[0-5]\d|60)?\b");
 
-            //Console.WriteLine("Task_4:");
-            /////Change only *text* on tag <em>text</em>
-            /////Pattern pass 7 test from 11
-            /////http://callumacrae.github.io/regex-tuesday/challenge4.html
-            ////// /\*([A-Za-z0-9 ,.]+)\*/g  <em>$1</em>
-            //FindAndChange(text,
-            //   @"\*([A-Za-z0-9 ]+)\*",
-            //   "<em>$1</em>");
+            Console.WriteLine("Task_4:");
+            ///Change only *text* on tag <em>text</em>      
+            ///http://callumacrae.github.io/regex-tuesday/challenge4.html
+            //// /\*([A-Za-z0-9 ,.]+)\*/g  <em>$1</em>
+            FindAndChange(text,
+               @"\*([A-Za-z0-9 ]+)\*",
+               "<em>$1</em>");
 
-            //Console.WriteLine("Task_5:");
-            ////Find numbers divided by ",."  8,205,500.4672 
-            ////Pattern pass 27 test from 31
-            ////Pattern:^\d{1,3}((,| )\d{3})*(.\d+)?$
-            ////https://callumacrae.github.com/regex-tuesday/challenge5.html
-            //FindMatches(text, @"\b\d{1,3}((,| )\d{3})*(.\d+)?\b");
+            Console.WriteLine("Task_5:");
+            //Find numbers divided by ",."  8,205,500.4672         
+            //Pattern:^\d{1,3}((,| )\d{3})*(.\d+)?$
+            //https://callumacrae.github.com/regex-tuesday/challenge5.html
+            FindMatches(text, @"\b\d{1,3}((,| )\d{3})*(.\d+)?\b");
 
-            ////Console.WriteLine("Task_6:");
-            ///////Find IPv4
-            ////FindMatches(text,
-            ////    @"");
+            Console.WriteLine("Task_6:");
+            ///Find IPv4 
+            ///Pattern:
+            ///http://callumacrae.github.com/regex-tuesday/challenge6.html
+            FindMatches(text,
+                @"");
 
-            //Console.WriteLine("Task_7:");
-            /////Test pass 38 test from 41
-            /////Pattern: /^(https|http)://(www\.)?[^-][\w-\.]{1,40}[^-.]\.\w{1,10}/?$/
-            /////https://callumacrae.github.com/regex-tuesday/challenge7.html
-            //FindMatches(text, @"\b(https|http)://(www\.)?[^-][\w-\.]{1,40}[^-.]\.\w{1,10}/?\b");
+            Console.WriteLine("Task_7:");
+            ///Find domains 
+            ///Pattern: /^(https|http)://(www\.)?[^-][\w-\.]{1,40}[^-.]\.\w{1,10}/?$/
+            ///https://callumacrae.github.com/regex-tuesday/challenge7.html
+            FindMatches(text, @"\b(https|http)://(www\.)?[^-][\w-\.]{1,40}[^-.]\.\w{1,10}/?\b");
 
-            //Console.WriteLine("Task_8:");
-            //FindAndChange(text,
-            //    @"\b([a-zA-Z'.\- ]+)+\s(\1)+\b",
-            //    @"*$1*");
+            Console.WriteLine("Task_8:");
+            ///Repeated point mark with *text*
+            ///Pattern:
+            ///http://callumacrae.github.io/regex-tuesday/challenge8.html
+            FindAndChange(text,
+                @"\b([a-zA-Z'.\- ]+)+\s(\1)+\b",
+                @"*$1*");
+
+       
              
 
         }
